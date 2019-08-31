12a944f31930a3754c08437f924be74d328494f3
田紳宏 <stefan@qraft.app>
2019-08-31 21:39:46 +0800

    Finished styling login-form

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
+import { Button, Icon } from '@material-ui/core';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { useMutation } from '@apollo/react-hooks';
 import { useAuth } from '../contexts/auth-context';
 import { useFormField } from '../custom-hooks/form';
+import FormikTextField from './FormikTextField';
+import boxShadow from './shared/boxShadow';
 
 const SIGN_UP = gql`
   mutation SignUp($name: String!, $email: String!, $password: String!) {
@@ -63,6 +67,35 @@ const initialLoginValues = {
   password: ''
 };
 
+const SeulForm = styled(Form)`
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+  padding: 30px;
+  border-radius: 15px;
+  ${boxShadow}
+`;
+
+const ButtonGroup = styled.div`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Error = styled.div`
+  color: red;
+  display: flex;
+  align-items: center;
+`;
+
+const ErrorIcon = styled(Icon)`
+  margin-right: 4px;
+  && {
+    font-size: 17px;
+  }
+`;
+
 const SignUp = () => {
   const name = useFormField('');
   const email = useFormField('');
@@ -72,7 +105,8 @@ const SignUp = () => {
   const [action, setAction] = useState({
     hasAccount: true,
     displayText: 'Need a new account?',
-    buttonText: 'login'
+    buttonText: 'login',
+    title: 'LOGIN'
   });
   const { updateCurrentUser } = useAuth();
 
@@ -104,33 +138,79 @@ const SignUp = () => {
         updateCurrentUser(user);
       }}
       render={({ errors, status, touched, isSubmitting }) => (
-        <Form>
+        <SeulForm>
+          <h3>{action.title}</h3>
           {!action.hasAccount && (
             <>
-              <Field type="name" name="name" />
-              <ErrorMessage name="name" component="div" />
+              <Field
+                type="name"
+                component={FormikTextField}
+                name="name"
+                error={errors && errors.name}
+              />
+              <ErrorMessage name="name">
+                {msg => (
+                  <Error>
+                    <ErrorIcon fontSize="small" color="error">
+                      error
+                    </ErrorIcon>
+                    {msg}
+                  </Error>
+                )}
+              </ErrorMessage>
             </>
           )}
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="div" />
-          <Field type="password" name="password" />
-          <ErrorMessage name="password" component="div" />
-          <button type="submit">{action.buttonText}</button>
-          <button
-            type="button"
-            onClick={() => {
-              setAction({
-                hasAccount: !action.hasAccount,
-                displayText: !action.hasAccount
-                  ? 'Need a new account?'
-                  : 'Already have an account?',
-                buttonText: !action.hasAccount ? 'login' : 'signup'
-              });
-            }}
-          >
-            {action.displayText}
-          </button>
-        </Form>
+          <Field
+            type="email"
+            component={FormikTextField}
+            name="email"
+            error={errors && errors.email}
+          />
+          <ErrorMessage name="email">
+            {msg => (
+              <Error>
+                <ErrorIcon fontSize="small" color="error">
+                  error
+                </ErrorIcon>
+                {msg}
+              </Error>
+            )}
+          </ErrorMessage>
+          <Field
+            type="password"
+            component={FormikTextField}
+            name="password"
+            error={errors && errors.password}
+          />
+          <ErrorMessage name="password">
+            {msg => (
+              <Error>
+                <ErrorIcon fontSize="small" color="error">
+                  error
+                </ErrorIcon>
+                {msg}
+              </Error>
+            )}
+          </ErrorMessage>
+          <ButtonGroup>
+            <Button type="submit">{action.buttonText}</Button>
+            <Button
+              type="button"
+              onClick={() => {
+                setAction({
+                  hasAccount: !action.hasAccount,
+                  displayText: !action.hasAccount
+                    ? 'Need a new account?'
+                    : 'Already have an account?',
+                  buttonText: !action.hasAccount ? 'login' : 'signup',
+                  title: !action.hasAccount ? 'LOGIN' : 'SIGNUP'
+                });
+              }}
+            >
+              {action.displayText}
+            </Button>
+          </ButtonGroup>
+        </SeulForm>
       )}
     />
   );
