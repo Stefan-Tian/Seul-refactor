6a219d32785f49ddef2769031c60f657aaf7eadd
田紳宏 <stefan@qraft.app>
2019-09-08 21:36:22 +0800

    Added relational field in resolvers

--- backend/src/resolvers/Task.js
+++ backend/src/resolvers/Task.js
@@ -2,6 +2,6 @@ const createdBy = (root, args, context) =>
   context.prisma.task({ id: root.id }).createdBy();
 
 const inProject = (root, args, context) =>
-  context.prisma.task({ id: root.id }).isProject();
+  context.prisma.task({ id: root.id }).inProject();
 
 export default { createdBy, inProject };
